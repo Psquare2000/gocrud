aa391e0c31e998758fc85a4ffb03c6e32e16705b
Psquare2000 <parthpushkar00@gmail.com>
2023-05-30 12:09:29 +0530

    fixed get by id

--- main.go
+++ main.go
@@ -92,10 +92,10 @@ func main() {
 	movies = append(movies, Movie{ID: "1", Isbn: "438227", Title: "movie one", Director: &Director{Firstname: "John", Lastname: "Doe"}})
 	movies = append(movies, Movie{ID: "2", Isbn: "45455", Title: "Movie Two", Director: &Director{Firstname: "Christopher", Lastname: "Nolan"}})
 	r.HandleFunc("/movies", getMovies).Methods("GET")
-	r.HandleFunc("movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
 	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
-	r.HandleFunc("movies/{id}", deleteMovie).Methods("DELETE")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Printf("Starting server at port 8000 \n")
 	log.Fatal(http.ListenAndServe(":8001", r))
